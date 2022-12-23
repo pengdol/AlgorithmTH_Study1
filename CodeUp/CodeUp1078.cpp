a845ab443ba656d305987b99dfa08f6df3c44080
HaeChan Jeong <108115672+harryputer1214@users.noreply.github.com>
2022-12-23 15:18:01 +0900

    Create CodeUp1078.cpp

---
+++ CodeUp/CodeUp1078.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+
+int main() {
+  int sum=0;
+	int i, n;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++)
+	{
+ 	  if(i%2==0) {
+			sum=sum+i;
+		}
+	}
+	printf("%d\n",sum);
+}

50d4c7b0cbb95a27b1bcae21b85e62ea698b4ab2
HaeChan Jeong <108115672+harryputer1214@users.noreply.github.com>
2023-01-27 17:48:35 +0900

    Create w5_팩토리얼.cpp

---
+++ w5_팩토리얼.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+
+int Factorial(int n){
+	if(n<=1){
+		return 1;
+	}
+	return n*Factorial(n-1);
+}
+
+int main() {
+  int n;
+	scanf("%d",&n);
+	printf("%d",Factorial(n));
+	return 0;
+}
